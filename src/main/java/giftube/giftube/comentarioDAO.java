291420fe967cef1ca00ae17ca3437e813ef02c03
David Padilla Montero <dpm00040@red.ujaen.es>
2020-03-17 16:37:29 +0100

    Base de datos preparada para su funcionamiento DAO comentario avanzado

---
+++ src/main/java/giftube/giftube/comentarioDAO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package giftube.giftube;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Id;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author Pacalor
+ */
+
+@RequestScoped
+@Transactional
+public class comentarioDAO {
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    private String comentario;
+    private List<Comentario> comentarios;
+    
+    private static final Logger logger = Logger.getLogger(comentarioDAO.class.getName());
+    
+    public List<Comentario> buscaTodos(){
+        try {
+             comentarios=em.createQuery("select c from Comentario c", Comentario.class).getResultList();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "No se pueden recuperar los libros", e);
+            comentarios = new ArrayList<>();
+        }
+        return comentarios;
+    }
+    public List<Comentario> buscaTodos(int gif){
+        try {
+             comentarios=em.createQuery("select c from Comentario c where c.gif_id = " +gif, Comentario.class).getResultList();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "No se pueden recuperar los libros", e);
+            comentarios = new ArrayList<>();
+        }
+        return comentarios;
+    }
+}
