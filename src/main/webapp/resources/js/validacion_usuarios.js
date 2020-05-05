6019c0ca228a227ee9a666f134f6993778709171
Miguel Angel Valero Domenech <mavd0002@red.ujaen.es>
2020-05-05 13:09:02 +0200

    Correción validacion JS para que se refresque tras un fallo

--- src/main/webapp/resources/js/validacion_usuarios.js
+++ src/main/webapp/resources/js/validacion_usuarios.js
@@ -18,31 +18,37 @@ function validarFormulario(event) {
     var contra1 = el('#fcliente\\:txtClave').value;
     var contra2 = el('#fcliente\\:txtConfirma').value;
     var valido = true;
-    if (nombre.length < 3 || nombre.length > 50) {
+    el('#errNombre').textContent = null;
+    el('#errApellidos').textContent = null;
+    el('#errUsuario').textContent = null;
+    el('#errClave').textContent = null;
+    el('#errConfirma').textContent = null;
+    
+    if (nombre.length < 3 || nombre.length > 20) {
         el('#errNombre')
-                .textContent = "La longitud del nombre no es válida";
+                .textContent = "La longitud del nombre no es válida [3, 20]";
         valido = false;
     }
     if (apellidos.length < 2 || apellidos.length > 30) {
         el('#errApellidos')
-                .textContent = "La longitud del apellido no es válida";
+                .textContent = "La longitud del apellido no es válida [2, 30]";
         valido = false;
     }
     if (usuario.length < 4 || usuario.length > 12) {
         el('#errUsuario')
-                .textContent = "La longitud del usuario no es válida";
+                .textContent = "La longitud del usuario no es válida [4, 12]";
         valido = false;
     }
     if (contra1.search(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm) === -1) {
         el('#errClave')
-                .textContent = "Clave no válida (Debe contener al menos 8 caracteres, debe contener al menos 1 letra mayuscula y 1 digito, puede contener caracteres especiales) ";
+                .textContent = "Clave no válida (Debe contener al menos 8 caracteres, al menos 1 letra mayúscula y 1 dígito, puede contener caracteres especiales) ";
         valido = false;
     }
 
 
     if (contra1 !== contra2) {
         el('#errConfirma')
-                .textContent = "La clave no coincide con la primera";
+                .textContent = "Las claves no coinciden";
         valido = false;
     }
 
